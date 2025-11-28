389d2a84fdd4ac00018e837ac4060fabc98140dd
CAI-JIADA <41343145@nfu.edu.tw>
2025-11-28 19:53:47 +0800

    end

--- tcpfilesever.h
+++ tcpfilesever.h
@@ -1,6 +1,8 @@
 #ifndef TCPFILESEVER_H
 #define TCPFILESEVER_H
 
+#include<QtNetwork>
+#include<QtWidgets>
 #include <QDialog>
 
 class TcpFilesever : public QDialog
@@ -10,5 +12,36 @@ class TcpFilesever : public QDialog
 public:
     TcpFilesever(QWidget *parent = nullptr);
     ~TcpFilesever();
+public slots:
+    void start();
+    void acceptConnection();
+    void updateServerProgress();
+    void displayError (QAbstractSocket:: SocketError socketError);
+private:
+    QProgressBar *serverProgressBar;
+    QLabel
+        *serverStatusLabel;
+    QPushButton
+        *startButton;
+    QPushButton
+        *quitButton;
+    QDialogButtonBox
+        *buttonBox;
+    QTcpServer
+        tcpServer;
+    QTcpSocket
+        *tcpServerConnection;
+    qint64
+        totalBytes;
+    qint64
+        byteReceived;
+    qint64
+        fileNameSize;
+    QString
+        fileName;
+    QFile
+        *localFile;
+    QByteArray
+        inBlock;
 };
 #endif // TCPFILESEVER_H
